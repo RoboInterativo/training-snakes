f811c53d8f9b1b5eb4fa9d5cbacb28581423a7ba
chiefexb <chief_exb@mail.ru>
2026-04-04 23:38:57 +0300

    123

--- app/snakes/snake_0.py
+++ app/snakes/snake_0.py
@@ -6,8 +6,27 @@ from .base_snake import BaseSnake
 
 class Snake0(BaseSnake):
 
+    # Old code
+    # def move(self, gamestate):
+    #
+    #     current_vector = gamestate.me.current_direction
+    #     if current_vector != noop:
+    #         return current_vector
+    #
+    #     head = gamestate.me.head
+    #     l_wall = Vector(0, head.y)
+    #     r_wall = Vector(gamestate.board_width-1, head.y)
+    #     t_wall = Vector(head.x, 0)
+    #     b_wall = Vector(head.x, gamestate.board_width-1)
+    #     farthest = head.farthest([l_wall, r_wall, t_wall, b_wall])
+    #     return {
+    #         l_wall: left,
+    #         r_wall: right,
+    #         t_wall: up,
+    #         b_wall: down,
+    #     }.get(farthest, up)
+    #newcode
     def move(self, gamestate):
-
         current_vector = gamestate.me.current_direction
         if current_vector != noop:
             return current_vector
@@ -17,38 +36,19 @@ class Snake0(BaseSnake):
         r_wall = Vector(gamestate.board_width-1, head.y)
         t_wall = Vector(head.x, 0)
         b_wall = Vector(head.x, gamestate.board_width-1)
-def move(self, gamestate):
-    current_vector = gamestate.me.current_direction
-    if current_vector != noop:
-        return current_vector
 
-    head = gamestate.me.head
-    l_wall = (0, head.y)           # Кортеж вместо Vector
-    r_wall = (gamestate.board_width-1, head.y)
-    t_wall = (head.x, 0)
-    b_wall = (head.x, gamestate.board_width-1)
+        farthest = head.farthest([l_wall, r_wall, t_wall, b_wall])
 
-    # Придётся изменить метод farthest, чтобы он работал с кортежами
-    # Или преобразовать векторы в кортежи
-    walls_as_vectors = [Vector(x, y) for (x, y) in [l_wall, r_wall, t_wall, b_wall]]
-    farthest = head.farthest(walls_as_vectors)
-
-    # Преобразуем farthest в кортеж для сравнения
-    farthest_tuple = (farthest.x, farthest.y)
-
-    return {
-        l_wall: left,
-        r_wall: right,
-        t_wall: up,
-        b_wall: down,
-    }.get(farthest_tuple, up)
-        # farthest = head.farthest([l_wall, r_wall, t_wall, b_wall])
-        # return {
-        #     l_wall: left,
-        #     r_wall: right,
-        #     t_wall: up,
-        #     b_wall: down,
-        # }.get(farthest, up)
+        # Простое сравнение (уже работает, т.к. есть __eq__)
+        if farthest == l_wall:
+            return left
+        if farthest == r_wall:
+            return right
+        if farthest == t_wall:
+            return up
+        if farthest == b_wall:
+            return down
+    return up
 
     def name(self):
         return "Training Snake 0"
