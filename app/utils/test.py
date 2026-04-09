1bbcd977d527d24ce715019c6799de7ab2f3e1ba
shilo.aleksey <shilo.aleksey@bsspharm.ru>
2026-04-09 15:41:07 +0300

    1

--- app/utils/test.py
+++ app/utils/test.py
@@ -23,23 +23,37 @@ def build_test_gamestate(width=3, height=3, me=[(0,0)], opponents=[], food=[]):
             "x": t[0],
             "y": t[1]
         } for t in tuples]
+    data= {
+    "game":{
+        "turn": 0
+    },
+    "board":
+     {
+      "food":  _tuples_to_coords(food),
+       "snakes": [_tuples_to_snake(coords) for coords in opponents],
+       "height": height,
+        "width": width,
 
-    data = {
-      "food": {
-        "data": _tuples_to_coords(food),
-        "object": "list"
-      },
-      "height": height,
-      "id": 1,
-      "object": "world",
-      "snakes": {
-        "data": [_tuples_to_snake(coords) for coords in opponents],
-        "object": "list"
-      },
-      "turn": 0,
-      "width": width,
-      "you": _tuples_to_snake(me)
+    },
+    "you": _tuples_to_snake(me)
     }
-    data["snakes"]["data"].append(data["you"])
+
+    # data = {
+    #   "food": {
+    #     "data": _tuples_to_coords(food),
+    #     "object": "list"
+    #   },
+    #   "height": height,
+    #   "id": 1,
+    #   "object": "world",
+    #   "snakes": {
+    #     "data": [_tuples_to_snake(coords) for coords in opponents],
+    #     "object": "list"
+    #   },
+    #   "turn": 0,
+    #   "width": width,
+    #   "you": _tuples_to_snake(me)
+    # }
+    data["board"]["snakes"].append(data["you"])
     gs = GameState(data)
     return gs
