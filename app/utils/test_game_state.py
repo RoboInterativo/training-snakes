dfde0e61c9893f2ec183aca5e5cd7cc606554f51
shilo.aleksey <shilo.aleksey@bsspharm.ru>
2026-04-14 10:18:29 +0300

    123

--- app/utils/test_game_state.py
+++ app/utils/test_game_state.py
@@ -46,6 +46,10 @@ def test_distance_to_turn_around():
     ]
     assert dists1 == expected1
 
+def test_you_nect():
+    gs = build_test_gamestate(1, 2, me=[(0, 1), (0, 2),(0,3)])
+    gs.me.neck== V(0, 2)
+
 # def test_real_gs():
 #     gs=GameState(real_json1)
 #     print(gs.me.head)
