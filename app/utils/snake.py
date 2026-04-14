63e3012962ab432f125b2e82fdb859745c9460e8
shilo.aleksey <shilo.aleksey@bsspharm.ru>
2026-04-14 10:11:27 +0300

    123

--- app/utils/snake.py
+++ app/utils/snake.py
@@ -7,7 +7,7 @@ class Snake(object):
         self.data = data
         self._coords = None
 
-
+#
     @property
     def coords(self):
         if self._coords is None:
@@ -18,7 +18,7 @@ class Snake(object):
     @property
     def head(self):
         return self.coords[0]
-        
+
 
     @property
     def neck(self):
